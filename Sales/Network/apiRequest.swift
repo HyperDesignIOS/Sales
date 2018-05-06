c9b771cddf434b6a01c611558dc216b4f2b50547
AmrObjection <>
2018-05-06 04:28:10 -0700

    add server manager

---
+++ Sales/Network/apiRequest.swift
@@ -0,0 +1,13 @@
+//
+//  apiRequest.swift
+//  Sales
+//
+//  Created by AmrObjection on 5/6/18.
+//  Copyright © 2018 AmrObjection. All rights reserved.
+//
+
+import Foundation
+
+class apiRequest {
+    
+}
